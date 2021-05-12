977c183311fbd1b4a628856d20a6e94175603b03
rakesh3899 <rakeshmistry3899@gmail.com>
2021-05-12 20:34:20 +0600

    jquery initializing and js basic

---
+++ 5.JS Basic/index.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    //$('h1').text('text cheanged');
+    $('h1').click(function () {
+        alert('Clicked');
+    });
+});
+// variable
+//var x;
+// string
+//var name = "namesdfsdf";
+// logical gate
+// and gate
+// var number = 31;
+// if (number > 33 && number < 80) {
+//     console.log('pass');
+// }
+//or
+// if (number > 33 || number < 80) {
+//     console.log('second pass');
+// }
+
+// not
+// if (number != 33) {
+//     console.log('not');
+// }
+///// condition
+// if (condition) { }
+// if (conditon) { }
+// else { }
+//
+// if (condition) { }
+// else if (condition) { }
+// else if (condition) { }
+// else { }
+
+// calculate gpa
+var marks = 55;
+
+if (marks >= 80) { console.log('A+'); }
+else if (marks >= 70) { console.log('A'); }
+else if (marks >= 60) { console.log('A-'); }
+else if (marks >= 50) { console.log('B'); }
+else if (marks < 33) { console.log('F'); }
+// switch

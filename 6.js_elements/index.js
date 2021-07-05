d0e895fb27f722e51650013e9d84262b1e2b1396
rakesh3899 <rakeshmistry3899@gmail.com>
2021-07-05 21:10:48 +0600

    js emelents details

---
+++ 6.js_elements/index.js
@@ -0,0 +1,46 @@
+
+// function sum(a, b = 0) {
+//     return a + b;
+// }
+// console.log(sum(6));
+
+//// array
+// var name = ['name 1', 'name 2', 'name 3'];
+// var roll = [101, 102, 103];
+// var gpa = [3.6, 3.4, 3.9];
+
+// // Name is name 1 & roll is 101 & gpa is 3.6
+// console.log("Name is " + name[2] + " & roll is " + roll[2] + " & gpa is " + gpa[2]);
+
+var marks = [50, 60, 45, 80, 85, 64, 90, 67, 54, 79, 60, 34, 80, 46, 345, 346, 565];
+//console.log(marks[0]);
+// 0 - 16
+//// loop
+
+// for loop
+
+/*
+
+*
+**
+***
+****
+*****
+
+*****
+****
+***
+**
+*
+
+*/
+for (var i = 0; i < marks.length; i++) {
+    console.log(marks[i]); //i =17
+}
+
+// while loop
+
+// foreach
+
+// map
+
